93d7f868f8c65d9a9b32f3f1b9e846b38b9f3a6e
xhui <xhui.xiao@gmail.com>
2019-06-10 21:51:06 +0800

    Rewrite InstantSell

--- InstantSell.py
+++ InstantSell.py
@@ -12,15 +12,16 @@ def InstantSell(stock, share):
         rst = api.Logon("125.39.80.105", 443, "184039030", "326326")
         if not rst:
             return u"连接服务器失败"
-    
+
     _stock = stock.encode()
     _share = int(share)
     
     ret_val = ""
     sp = StockPool(api)
-    sp.sync()
     cache = Cache(api)
     _raising_price = cache.get(_stock, "涨停价")
+
+    # 取消订单
     c,d,e = sp.acquire(_stock, _share)
     if not c:
         return u"证券数量不足"
@@ -30,63 +31,104 @@ def InstantSell(stock, share):
             sp.removeOrder(i)
     else:
         return u"撤消订单失败"
-    
-    time.sleep(0.5)
-    # 获取即时价格
-    rst = api.Query("行情",stock=_stock)
-    if not rst:
-        return u"获取行情失败"
 
-    instant_price = round_up_decimal_2(float(rst[0][0][5]))
-    bid_1 = round_up_decimal_2(float(rst[0][0][16]))
-    print u"现价",instant_price
-    print u"卖一价",bid_1
-    if instant_price > bid_1:
-        _price = bid_1
-    else:
-        _price = instant_price
+##    if e != 0:
+##        time.sleep(0.3)
+##        rst = sp.fill(_stock, e)
+##        if not rst:
+##            ret_val += u"股票池拆单抢回失败"
 
-    # 下单
-    ret_val += u" 价格 " + str(_price) + u" "
-    if e == 0:
-        rst = api.Short(_stock, _price, _share)
-        # 若下单失败，重新用涨停价抢单
-        if not rst:
-            api.Short(_stock, _raising_price, _share)
-            return ret_val + u"失败"
-    else:
-        rst = api.SendOrders([3,3],[_stock,_stock],[_price,_raising_price],[_share,e])
-        # 若下单失败，重新用涨停价抢单
-        if not rst:
-            if not rst[0]: # 订单失败
-                api.Short(_stock, _raising_price, _share)
-            if not rst[1]: # 拆单后抢回失败
-                api.Short(_stock, _raising_price, e)
-            return ret_val + u"失败"
+    time.sleep(0.3)
+    for try_times in range(3):
+        # 获取即时价格
+        _price = getProperPrice(_stock)
+        # 下单
+        ret_val += u" 价格 " + str(_price) + u" "
+        if e == 0:
+            rst = api.Short(_stock, _price, _share)
+            # 若下单失败，重新用涨停价抢单
+            if not rst:
+                time.sleep(0.3)
+                ret_val += u" 下单失败，抢回 "
+                rst = sp.fill(_stock, _share)
+                if rst:
+                    ret_val += str(_share)
+                else:
+                    ret_val += u"失败"
+                return ret_val
+        else:
+            rst = api.SendOrders([3,3],[_stock,_stock],[_price,_raising_price],[_share,e])
+            # 若下单失败，重新用涨停价抢单
+            if not rst:
+                time.sleep(0.3)
+                ret_val += u"2 下单失败，抢回 "
+                refill_shares = 0
+                if not rst[0]: # 订单失败
+                    refill_shares += _share
+                if not rst[1]: # 拆单后抢回失败
+                    refile_shares += e
+                rst = sp.fill(_stock, refill_shares)
+                if rst:
+                    ret_val += str(e)
+                else:
+                    ret_val += u"失败"
+                return ret_val
 
-    order_id = rst[0][0][0]
+        order_id = rst[0][0][0]
+        e = 0 # 已经抢回，不需要重复抢单
 
-    time.sleep(0.5)
-    rst = api.Query("当日委托")
-    if rst:
-        if order_id in rst[0]["委托编号"]:
+        # 查询委托单是否为废单，若是废单抢回
+        time.sleep(0.5)
+        status = checkOrderStatus(order_id)
+        if status == "废单":
+            continue
+        if status == "已报":
+            api.CancelOrder(order_id)
+            continue
+        if status == "已成":
+            ret_val += u"成功 " + str(order_id)
+            break
+
+    return ret_val
+
+def checkOrderStatus(order_id, count = 3, check_interval=0.1):
+    for i in range(count):
+        rst = api.Query("当日委托")
+        if rst and order_id in rst[0]["委托编号"]:
             index = rst[0]["委托编号"].index(order_id)
             status = rst[0]["状态说明"][index]
-            if status == "废单":
-                rst = api.Short(_stock, _price, _share)
-                if rst:
-                    return ret_val + u"废单 " + order_id + u" 重新下单"
-                else:
-                    rst = api.Short(_stock, _raising_price, _share)
-                    if rst:
-                        return ret_val + u"废单 " + order_id + u" 重新下单，失败; 抢回 " + str(_share)
-                    else:
-                        return ret_val + u"废单 " + order_id + u" 重新下单，失败; 抢回失败"
+            return status
+        time.sleep(check_interval)
 
-    return ret_val + u"成功"
+def getProperPrice(stock):
+    # 获取最合适价格
+    lv2 = Lv2Api()
+    rst = lv2.GetQuotes5(stock)
+    if not rst:
+        return u"获取行情失败"
+    # 现价
+    instant_price = round_up_decimal_2(float(rst[0][0][3]))
+    bid_1 = round_up_decimal_2(float(rst[0][0][18]))
+    buy_1 = round_up_decimal_2(float(rst[0][0][17]))
+    print u"现价",instant_price
+    print u"卖一价",bid_1
+    print u"买一价", buy_1
+    if instant_price > bid_1:
+        price = bid_1
+    else:
+        price = instant_price
+    return price
 
 if __name__ == "__main__":
-    for i in range(6):
-        print InstantSell("000425", 100)
-        time.sleep(10)
+    api = TradeApi()
+    if not api.isLogon():
+        rst = api.Logon("125.39.80.105", 443, "184039030", "326326")
+    
+    sp = StockPool(api)
+    cache = Cache(api)
+    sp.sync()
+    for i in range(5):
+        print InstantSell("600875", 100)
+        sp.sync()
+        time.sleep(5)
     
