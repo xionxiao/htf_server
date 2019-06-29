241f2bd8961da546abb9d5ca313d885f26fdc9ec
xhui <xhui.xiao@gmail.com>
2019-06-29 14:28:40 +0800

    Display Quotes 10 in MarketApi.py

--- MarketApi.py
+++ MarketApi.py
@@ -86,17 +86,48 @@ class MarketApi(Singleton):
         self.Disconnect()
 
 if __name__ == "__main__":
+    import os, msvcrt, signal
+    def OnExit(signum, frame):
+        print u"按任意键退出"
+        msvcrt.getch()
+        lv2.Disconnect()
+        exit()
+
+    signal.signal(signal.SIGINT, OnExit)
     lv2 = MarketApi.Instance()
     print lv2.Connect("61.135.142.90",443)
-    for i in range(1):
-        print "================= ",time.strftime("%H:%M:%S")
-        rst = lv2.GetQuotes5('300001')
-        print u"现价", rst[0][0][3]
-        print u"买一",rst[0][0][17],rst[0][0][19]
-        print u"卖一",rst[0][0][18],rst[0][0][20]
-        #time.sleep(1)
+    while(True):
+        os.system('cls')
+        out_str = u"==== "
+        out_str += time.strftime("%H:%M:%S")
+        out_str += "\n"
+        rst = lv2.GetQuotes10('601699')
+        out_str += u"现价 " + rst[0][0][3][:-4] + "\n"
+        out_str += u"--\n"
 
-    for i in range(2):
-        printd(lv2.GetMinuteTimeData('601699')[0])
-        time.sleep(1)
+        out_str += u"卖十 " + rst[0][0][59][:-4] + " " + rst[0][0][61] + "\n"
+        out_str += u"卖九 " + rst[0][0][55][:-4] + " " + rst[0][0][57] + "\n"
+        out_str += u"卖八 " + rst[0][0][51][:-4] + " " + rst[0][0][53] + "\n"
+        out_str += u"卖七 " + rst[0][0][47][:-4] + " " + rst[0][0][49] + "\n"
+        out_str += u"卖六 " + rst[0][0][43][:-4] + " " + rst[0][0][45] + "\n"
+        out_str += u"卖五 " + rst[0][0][34][:-4] + " " + rst[0][0][36] + "\n"
+        out_str += u"卖四 " + rst[0][0][30][:-4] + " " + rst[0][0][32] + "\n"
+        out_str += u"卖三 " + rst[0][0][26][:-4] + " " + rst[0][0][28] + "\n"
+        out_str += u"卖二 " + rst[0][0][22][:-4] + " " + rst[0][0][24] + "\n"
+        out_str += u"卖一 " + rst[0][0][18][:-4] + " " + rst[0][0][20] + "\n"
+        out_str += u"--\n"
+        out_str += u"买一 " + rst[0][0][17][:-4] + " " + rst[0][0][19] + "\n"
+        out_str += u"买二 " + rst[0][0][21][:-4] + " " + rst[0][0][23] + "\n"
+        out_str += u"买三 " + rst[0][0][25][:-4] + " " + rst[0][0][27] + "\n"
+        out_str += u"买四 " + rst[0][0][29][:-4] + " " + rst[0][0][31] + "\n"
+        out_str += u"买五 " + rst[0][0][33][:-4] + " " + rst[0][0][35] + "\n"
+        out_str += u"买六 " + rst[0][0][42][:-4] + " " + rst[0][0][44] + "\n"
+        out_str += u"买七 " + rst[0][0][46][:-4] + " " + rst[0][0][48] + "\n"
+        out_str += u"买八 " + rst[0][0][50][:-4] + " " + rst[0][0][52] + "\n"
+        out_str += u"买九 " + rst[0][0][54][:-4] + " " + rst[0][0][56] + "\n"
+        out_str += u"买十 " + rst[0][0][58][:-4] + " " + rst[0][0][60] + "\n"
+        
+        print out_str
+        time.sleep(0.5)
+
     lv2.Disconnect()
